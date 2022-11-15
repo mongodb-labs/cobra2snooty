fdfce4cf4d74100cfee30be5443ff1230be5a239
sarahsimpers <82042374+sarahsimpers@users.noreply.github.com>
2022-11-15 06:31:11 -0500

    DOCSP-26295: Fixes incorrect spacing for commands without # (#27)

--- cobra2snooty_test.go
+++ cobra2snooty_test.go
@@ -71,7 +71,7 @@ func Echo() *cobra.Command {
 		Aliases: []string{"say"},
 		Short:   "Echo anything to the screen",
 		Long:    "an utterly useless command for testing",
-		Example: " # Example with intro text\n  atlas command no intro text\n",
+		Example: " # Example with intro text\n atlas command no intro text\n",
 		Annotations: map[string]string{
 			"string to printDesc": "A string to print",
 			"test paramDesc":      "just for testing",
@@ -129,10 +129,9 @@ func TestGenDocs(t *testing.T) {
 	output := buf.String()
 
 	checkStringContains(t, output, Echo().Long)
-	checkStringContains(t, output, `.. code-block::
-
-   # Example with intro text
-   atlas command no intro text
+	checkStringContains(t, output, `
+  # Example with intro text
+  atlas command no intro text
 `)
 	checkStringContains(t, output, "boolone")
 	checkStringContains(t, output, "rootflag")
@@ -158,10 +157,9 @@ func TestGenDocsNoHiddenParents(t *testing.T) {
 	output := buf.String()
 
 	checkStringContains(t, output, Echo().Long)
-	checkStringContains(t, output, `.. code-block::
-
-   # Example with intro text
-   atlas command no intro text
+	checkStringContains(t, output, `
+  # Example with intro text
+  atlas command no intro text
 `)
 	checkStringContains(t, output, "boolone")
 	checkStringOmits(t, output, "rootflag")

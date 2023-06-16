6b60367b675f725c877b87fc9521082cdeb33425
Sarah Simpers <sarah.simpers@mongodb.com>
2023-06-16 06:45:32 -0400

    Moves variables inside if statement

--- flags.go
+++ flags.go
@@ -73,13 +73,13 @@ func FlagUsages(f *pflag.FlagSet) string {
 			}
 		}
 
-		mutuale := fmt.Sprintf("%v", flag.Annotations["cobra_annotation_mutually_exclusive"])
-		mutuale = strings.ReplaceAll(mutuale, "]", "")
-		mutuale = strings.ReplaceAll(mutuale, "[", "")
-		mutuale = strings.ReplaceAll(mutuale, flag.Name+" ", "")
-		mutuale = strings.ReplaceAll(mutuale, " "+flag.Name, "")
-		mutuale = strings.ReplaceAll(mutuale, " ", ", --")
 		if len(flag.Annotations["cobra_annotation_mutually_exclusive"]) != 0 {
+			mutuale := fmt.Sprintf("%v", flag.Annotations["cobra_annotation_mutually_exclusive"])
+			mutuale = strings.ReplaceAll(mutuale, "]", "")
+			mutuale = strings.ReplaceAll(mutuale, "[", "")
+			mutuale = strings.ReplaceAll(mutuale, flag.Name+" ", "")
+			mutuale = strings.ReplaceAll(mutuale, " "+flag.Name, "")
+			mutuale = strings.ReplaceAll(mutuale, " ", ", --")
 			line += "\n    - " + usage + "\n\n      Mutually exclusive with --" + mutuale + "."
 		} else {
 			line += "\n    - " + usage
